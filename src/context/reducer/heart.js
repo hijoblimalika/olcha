2e3906b646cccdf77f9eca184818ab4bcfde11be
hijoblimalika <nematxanovanaima@gmail.com>
2023-02-08 14:35:58 +0500

    lorem ipsume

--- src/context/reducer/heart.js
+++ src/context/reducer/heart.js
@@ -1,7 +1,11 @@
+import { ADD_TO_LIKE, REMOVE_LIKE } from "../action/actionType"
+
 const heart = (state=[], action)=>{
     switch(action.type){
         case "ADD_TO_LIKE" :
             return state = [...state, action.payload]
+            case REMOVE_LIKE :
+                return state = state.filter( obj=> obj.id !== action.payload )
         default :
         return state
     }
--- src/router/products/Products.js
+++ src/router/products/Products.js
@@ -2,30 +2,47 @@ import React from 'react'
 import { PRODUCTS } from "../../static"
 import { Link } from 'react-router-dom'
 import "./Products.css"
-import {AiOutlineHeart} from "react-icons/ai" 
-import { useDispatch } from "react-redux"
+import {AiOutlineHeart, AiFillHeart } from "react-icons/ai" 
+import { useDispatch, useSelector } from "react-redux"
 import { ADD_TO_LIKE } from "../../context/action/actionType"
 
 function Products() {
   const dispatch = useDispatch()
+  const likes = useSelector(s=>s.heart)
+  
+  const addHeart = (item)=>{
+    let index = likes.findIndex(i=> i.id === item.id)
+    if (index > -1) {
+      return
+    }
+    dispatch({type:ADD_TO_LIKE, payload: item})
+  }
   return (
     <div className="container">
 
     <div className='products'>
         {
             PRODUCTS?.map((item, inx)=> <div  key={inx} className="product__item">
-              <AiOutlineHeart onClick={()=> dispatch({type:ADD_TO_LIKE, payload: item})} className='pro__heart'/>
+              <div onClick={()=> addHeart(item)} className='pro__heart'>
+                {
+                  likes?.some(i => i.id === item.id ) ? <AiFillHeart/> : <AiOutlineHeart/>
+                }
+              </div>
+
+              <div>
+
+              </div>
               <Link to={`/products/${item.id}`}>
                 <img src={item?.urls?.[0]} alt="" />
               
               </Link>
-                <p className='p'>{item?.title}</p>
+                <p className='pro__title'>{item?.title}</p>
                 <br />
                 <br />
                 <br />
                 <b >{item?.price} So'm</b>
                 <div className="mtolov">
-                  <div>{item?.muddatlitolov?.[1]}</div>
+                  <div className='mtolov2'>{item?.muddatlitolov?.[1]}</div>
                 </div>
                 <button className='pro__cart'>Add to Cart</button>
             </div>)
