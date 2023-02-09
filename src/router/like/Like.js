eedbe0074010db7d7176341999113684e27b9ab8
hijoblimalika <nematxanovanaima@gmail.com>
2023-02-09 22:38:44 +0500

    like media tayyor

--- src/router/like/Like.js
+++ src/router/like/Like.js
@@ -4,38 +4,37 @@ import Empty from "../../components/empty/Empty"
 import { useSelector } from 'react-redux'
 
 
-function Like() {
+    function Like() {
 const like = useSelector(s=> s.heart)
 console.log(like)
-return (
-<div className='container like'>
-{
-like.length === 0 ? <Empty url="https://olcha.uz/_nuxt/empty-img.3a4aef3b.png" text="Sevimli mahsulotlar yo'q"/>
-:
-<div>
-{
-like?.map((item, inx)=><div key={inx} className="like__card">
-<img className='like__imgs' src={item?.urls[0]} alt="" />
-<p>{item?.title}</p>
-<div>
+    return (
+    <div className='container like'>
+    {
+    like.length === 0 ? <Empty url="https://olcha.uz/_nuxt/empty-img.3a4aef3b.png" text="Sevimli mahsulotlar yo'q"/>
+    :
+    <div>
+    {
+    like?.map((item, inx)=><div key={inx} className="like__card">
+    <img className='like__imgs' src={item?.urls[0]} alt="" />
+    <p className='two__title'>{item?.title}</p>
+    <div className='two__container'>
 
-<del className='two'>
-{item?.price + 200} so'm</del>
-<br />
-<p>{item?.price} so'm</p>
-</div>
+    <del className='two'>
+    {item?.price + 200} so'm</del>
+    <br />
+    <p className='two2'>{item?.price} so'm</p>
+    </div>
 
-<button></button>
-<div className='like__btn'>
-<button className='like__btn2'>savatchaga qoshish</button>
-<button className='like__btn3'>O'chirish</button>
-</div>
-</div>)
-}
-</div>
-}
-</div>
+    <div className='like__btn'>
+    <button className='like__btn2'>savatchaga qoshish</button>
+    <button className='like__btn3'>O'chirish</button>
+    </div>
+    </div>)
+    }
+    </div>
+    }
+    </div>
 
-)
-}
-export default Like
+    )
+    }
+    export default Like
